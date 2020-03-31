9a851da998744a7754b803339f6f9e3bb01c7290
masi2k203 <57385580+masi2k203@users.noreply.github.com>
2020-03-31 20:15:17 +0900

    UI要素の配置

--- VScodeIntelliSenseForPIC/MainWindow.xaml.cs
+++ VScodeIntelliSenseForPIC/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace VScodeIntelliSenseForPIC
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : NavigationWindow
     {
         public MainWindow()
         {
